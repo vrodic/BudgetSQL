ace8cf07bbb02d06d8eec8e61965e078c021289a
Vedran Rodic <vrodic@gmail.com>
2010-08-29 12:19:15 +0200

    - add graphical progress

--- www/index.php
+++ www/index.php
@@ -2,8 +2,18 @@
 <head> 
 <title>Budget for Croatia</title> 
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+    <script type="text/javascript" src="include/js/jquery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="include/js/jquery-ui-1.8.4.custom.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="include/css/ui-lightness/jquery-ui-1.8.4.custom.css" /> 
 </head>
 <body>
+    
+
+<div class="demo">
+
+	<div id="progressbar"></div>
+
+</div><!-- End demo -->
 <b>Proračun</b><br>   
 <form action=index.php method="GET">
     Pretraži: <input type="text" name="nameq" value="">
@@ -23,12 +33,19 @@ require("include/dbcon.inc");
 function fmoney($num) {
     return number_format($num,0,".",",");
 }
+
+function getDetailByCode($code) {
+     $sql = "SELECT id, name, code,amount1,amount2,amount3 FROM MainItems WHERE code='$code'";
+    return pg_query($sql);
+}
 setlocale(LC_ALL,"en_US.utf8");
 $parent = $_GET["parent"];
 $codeq = $_GET["codeq"];
 $nosubitem = $_GET["nosubitem"];
 $parentfine = $_GET["parentfine"];
 $typecode = $_GET["typecode"];
+$orderf = $_GET["orderf"];
+$orderv = $_GET["orderv"];
 $code = $_GET["code"];
 $nameq = mb_strtoupper($_GET["nameq"]);
 $nameq = strtr($nameq, 'čćžšđ','ČĆŽŠĐ' ); // if anybody knows a better alternative to uppercase croatian characters, please shout 
@@ -53,6 +70,16 @@ if ($parent == 0 || $parent == -1) {
     $order =" id ASC";
 }
 
+if ($orderf) {
+    if (!$orderv) $orderv = "DESC";
+    $order = " amount$orderf $orderv";
+    if ($orderv == "DESC") {
+        $orderv = "ASC";
+    } else {
+        $orderv = "DESC";
+    }
+}
+
 if ($parentfine) {
     $where .= "and parentfine='$parentfine' ";
 } else if ((! $code && ! $codeq &&  $parent < 0) || $nosubitem){
@@ -74,13 +101,19 @@ if ($parentfine) {
     $res = pg_query($sql);
         echo $sql."<br>";
    
+
+$totalp = 0;
+$totalph = "";
     
 
 if ($parent > 0) {
         $sql = "SELECT id, name, code,amount1,amount2,amount3 FROM MainItems WHERE id='$parent'";
         $res3 = pg_query($sql);
         $pname = pg_result($res3,0,1);
-        echo "<b>$pname</b><br>";
+        echo "<br><b>$pname</b><br>";
+              $totaluf = pg_result($res3,0,3);
+        $total = fmoney($totaluf);
+        $totalp = 1;
 }
 if ($parentfine) {
         $sql = "SELECT id, name, code,amount1,amount2,amount3 FROM MainItems WHERE code='$parentfine'";
@@ -89,23 +122,31 @@ if ($parentfine) {
         echo "<b>$pname</b><br>";
 }
 
-if ($parent < 1) {
+if ($parent < 1 && ! $parentfine) {
+    $totalp = 1;
  $sql = "SELECT sum(amount1),sum(amount2),sum(amount3)  FROM MainItems $fullwhere ";
    
     $res2 = pg_query($sql);
-    $total = fmoney(pg_result($res2,0,0));
+    $totaluf = pg_result($res2,0,0);
+    $total = fmoney($totaluf);
 }
     ?>
     
     
     <table>
         <?
-        if ($parent < 1) {
-        echo "<tr><td></td><td></td><td align=right><b>Total:</b>$total<br></td><td></td><td></td></tr>";
+        if ($totalp) {
+        echo "<tr><td></td><td></td><td></td><td align=right><b>Total: </b>$total<br></td><td></td><td></td></tr>";
+        $totalph = "<td>% od ukupnog </td>";
         }
         ?>
         
-    <tr><td><b>Šifra</b></td><td><b>Naziv stavke</b></td><td align=right><b>Iznos 2010</b></td><td align=right><b>Iznos 2011</b></td><td align=right><b>Iznos 2012</b></td></b></tr>
+    <tr><td><b>Šifra</b></td><td><b>Naziv stavke</b></td><? echo $totalph;?><td align=right><b>
+    <?
+    echo "<a href='index.php?parent=$parent&parentfine=$parentfine&typecode=$typecode&code=$code&nameq=$nameq&codeq=$codeq&orderf=1&orderv=$orderv'>
+    Iznos 2010</a>";
+    ?>
+    </b></td><td align=right><b>Iznos 2011</b></td><td align=right><b>Iznos 2012</b></td></b></tr>
     <?
     $cnt = 0;
     while ($row = pg_fetch_assoc($res)) {
@@ -117,6 +158,7 @@ if ($parent < 1) {
         }
          $parenta = $row['parent'];
           $codea = $row['code'];
+          $magic = substr($codea,0,1);
           $parentfinea = $row['parentfine'];
           if ($parent == -1 ) {
            
@@ -130,8 +172,7 @@ if ($parent < 1) {
         echo "<tr><td bgcolor=$col>$pcode</td><td  bgcolor=$col><b>".$ahref.$pname.$aterm."</b></td></tr>";
           
           if (! $codeq) {
-            $sql = "SELECT id, name, code,amount1,amount2,amount3 FROM MainItems WHERE code='$parentfinea'";
-            $res3 = pg_query($sql);
+            $res3 = getDetailByCode($parentfinea);
             $pname = pg_result($res3,0,1);
             $pcode = pg_result($res3,0,2);
             $ahref ="<a href='index.php?parent=-2&parentfine=".$parentfinea."'>";
@@ -143,7 +184,7 @@ if ($parent < 1) {
         echo "<td bgcolor=$col>";
         $aterm = "";
         $ahref = "";
-        if ($row['subitem'] == 0) {
+        if ($row['subitem'] == 0 && ($magic == "A" || $magic == "K")) {
         if ($parent > 0 || $parent < 0) {
             $ahref ="<a href='index.php?parent=-2&parentfine=".$row['code']."'>";
             $aterm = "</a>";
@@ -152,16 +193,39 @@ if ($parent < 1) {
             $aterm = "</a>";
         }
         }
+        if ($row['subitem'] == 0 && $magic != "A" && $magic !="K") {
+            
+            $ahref = "<b>";
+            $aterm = "</b>";
+        }
+        
         $name = $row['name'];
         $spcname= "";
         if($row['subitem'] == "1") {
+            
+            if ($orderf) {
+                $res3 = getDetailByCode($parentfinea);
+                $pname = pg_result($res3,0,1);
+                $name = "$name &nbsp;&nbsp; $pname";
+            }
             $spcname = "&nbsp;&nbsp;";
+            
         } 
        
       echo $spcname.$ahref.$name."$aterm</td>";
       $amount1 = fmoney($row['amount1']);
       $amount2 = fmoney($row['amount2']);
       $amount3 = fmoney($row['amount3']);
+      if ($totalp) {
+        $percent = ($row['amount1']/$totaluf)*100;
+        $js .= "$(\"#pbar$cnt\").progressbar({
+			value: $percent
+		});
+                $(\"#pbar$cnt\").height(20);
+                ";
+        echo "<td><div id='pbar$cnt'></div>".number_format($percent,2)."</td>";  
+      }
+      
   echo "<td align='right'  bgcolor=$col>".$amount1."</td>";
   echo "<td align='right' bgcolor=$col>".$amount2."</td>";
   echo "<td align='right' bgcolor=$col>".$amount3."</td>";
@@ -171,5 +235,13 @@ if ($parent < 1) {
 
 
 ?>
+    	
+	<script type="text/javascript">
+	$(function() {
+            <? echo $js;?>
+		
+	});
+	</script>
+
 </body>
 </html>
